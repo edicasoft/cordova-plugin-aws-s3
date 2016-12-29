23fbd4920bb4e5b06a34d2a3a6891be757099ad4
Kunio Sawatani <devel@fathens.org>
2016-12-29 10:53:13 +0900

    Android の実装を完了

--- platforms/android/src/main/kotlin/AwsS3.kt
+++ platforms/android/src/main/kotlin/AwsS3.kt
@@ -4,32 +4,24 @@ import android.content.pm.PackageManager.GET_META_DATA
 import android.os.Bundle
 import com.amazonaws.auth.AWSCredentialsProvider
 import com.amazonaws.auth.CognitoCachingCredentialsProvider
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferState
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility
 import com.amazonaws.regions.Regions
 import org.apache.cordova.CallbackContext
 import org.apache.cordova.CordovaPlugin
 import org.json.JSONArray
 import org.json.JSONObject
 import com.amazonaws.services.s3.AmazonS3Client
-import com.amazonaws.services.s3.model.DeleteObjectRequest
 import com.amazonaws.services.s3.model.DeleteObjectsRequest
 import com.amazonaws.services.s3.model.ObjectMetadata
+import org.apache.cordova.PluginResult
 import java.io.File
 
 public class AwsS3 : CordovaPlugin() {
     private class PluginContext(val holder: AwsS3, val action: String, val callback: CallbackContext) {
         fun error(msg: String?) = callback.error(msg)
-        fun success() = callback.success(null as? String)
-        fun success(msg: String?) = callback.success(msg)
-        fun success(obj: JSONObject?) {
-            if (obj != null) {
-                callback.success(obj)
-            } else {
-                success()
-            }
-        }
+        fun success(msg: String? = null) = callback.success(msg)
+        fun success(v: Boolean) = callback.sendPluginResult(PluginResult(PluginResult.Status.OK, v))
+        fun success(m: Map<*, *>) = callback.success(JSONObject(m))
+        fun success(list: List<*>) = callback.success(JSONArray(list))
     }
 
     private var context: PluginContext? = null
@@ -87,13 +79,13 @@ public class AwsS3 : CordovaPlugin() {
     }
 
     fun write(args: JSONArray) {
-        val file = File(args.getString(1))
-        s3.putObject(bucketName, args.getString(0), file)
+        s3.putObject(bucketName, args.getString(0), File(args.getString(1)))
         context?.success()
     }
 
     fun remove(args: JSONArray) {
         s3.deleteObject(bucketName, args.getString(0))
+        context?.success()
     }
 
     fun removeFiles(args: JSONArray) {
@@ -102,11 +94,62 @@ public class AwsS3 : CordovaPlugin() {
         }
         val req = DeleteObjectsRequest(bucketName).withKeys(keys)
         s3.deleteObjects(req)
+        context?.success()
+    }
+
+    fun removeDir(args: JSONArray) {
+        s3.deleteObjects(DeleteObjectsRequest(bucketName).withKeys(
+                list(args.getString(0)).map {
+                    DeleteObjectsRequest.KeyVersion(it)
+                }
+        ))
+        context?.success()
+    }
+
+    fun copy(args: JSONArray) {
+        s3.copyObject(bucketName, args.getString(0), bucketName, args.getString(1))
+        context?.success()
+    }
+
+    fun move(args: JSONArray) {
+        move(args.getString(0), args.getString(1))
+    }
+
+    fun moveDir(args: JSONArray) {
+        val srcDir = args.getString(0)
+        val dstDir = args.getString(1)
+        list(srcDir).map { src ->
+            move(src, src.replace(srcDir, dstDir))
+        }
+        context?.success()
     }
 
     fun list(args: JSONArray) {
-        val res = s3.listObjects(bucketName, args.getString(0))
-        res.
+        val keys = list(args.getString(0))
+        context?.success(keys)
+    }
+
+    fun exists(args: JSONArray) {
+        val res = s3.doesObjectExist(bucketName, args.getString(0))
+        context?.success(res)
+    }
+
+    fun url(args: JSONArray) {
+        val res = s3.getUrl(bucketName, args.getString(0))
+        context?.success(res.toString())
+    }
+
+    // private impl
+
+    fun move(src: String, dst: String) {
+        s3.copyObject(bucketName, src, bucketName, dst)
+        s3.deleteObject(bucketName, src)
+    }
+
+    fun list(path: String): List<String> {
+        val dir = if (path.endsWith("/")) path else "${path}/"
+        val res = s3.listObjects(bucketName, dir)
+        return res.objectSummaries.map { it.key }
     }
 
     // private utilities
