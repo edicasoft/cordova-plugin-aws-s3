29fc24606f6b87afc599cc85084b7ff0bc9d0698
Kunio Sawatani <devel@fathens.org>
2016-12-30 11:27:01 +0900

    Javascript のインターフェースを作成

---
+++ www/s3.js
@@ -0,0 +1,32 @@
+var cordova = require("cordova/exec");
+
+var pluginName = "AWSS3Plugin"
+
+var names = [
+"download",
+"read",
+"upload",
+"write",
+"remove",
+"removeFiles",
+"removeDir",
+"copy",
+"move",
+"moveDir",
+"list",
+"exists",
+"url"
+];
+
+var obj = {};
+
+names.forEach(function(methodName) {
+    obj[methodName] = function() {
+        var args = Array.prototype.slice.call(arguments, 0);
+        return new Promise(function(resolve, reject) {
+            cordova(resolve, reject, pluginName, methodName, args);
+        });
+    }
+});
+
+module.exports = obj;
